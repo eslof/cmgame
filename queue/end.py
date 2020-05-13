87ce25bd9d431f9b33033e828f2026a714d92f9c
eslof <eslof.github@gmail.com>
2020-05-13 20:47:14 +0200

    Further TODO updates for Queue
    unsure if validation for this operation is even needed which begins to question whether the RequestHandler ABC was a mistake

--- queue/end.py
+++ queue/end.py
@@ -2,7 +2,14 @@ from internal import RequestHandler
 
 
 class End(RequestHandler):
+
     @staticmethod
     def run(user_id: str) -> None:
         # TODO: implement
+        pass
+
+    @staticmethod
+    def validate(*args, **kwargs):
+        # TODO: implement
+        # we'll see if this is even necessary
         pass
