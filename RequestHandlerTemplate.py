feb7cdb4475abfd0ad03e92d5a7432ac7338bdcc
eslof <eslof.github@gmail.com>
2020-05-15 23:33:21 +0200

    Template for new RequestHandler implementation
    Tools -> Save File as Template... -> OK!

---
+++ RequestHandlerTemplate.py
@@ -0,0 +1,15 @@
+from internal import RequestHandler
+#set($camel_case_name = ${StringUtils.removeUnderScores($NAME)})
+
+class $camel_case_name(RequestHandler):
+    """$camel_case_name documentation"""
+
+    @staticmethod
+    def run(*args, **kwargs):
+        """Run documentation"""
+        pass
+
+    @staticmethod
+    def validate(*args, **kwargs):
+        """Validate documentation"""
+        pass
