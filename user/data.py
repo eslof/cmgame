81eb6419db56b26a1534960cd504bbda3b177c15
eslof <eslof.github@gmail.com>
2020-05-15 21:47:59 +0200

    Data request clean up
    trycatch is built into User.get so no need
    
    this is so clean

--- user/data.py
+++ user/data.py
@@ -16,34 +16,22 @@ class Data(RequestHandler):
 
     @staticmethod
     def validate():
-        """TODO: figure out what to do here since we're already auth'd"""
+        """TODO: figure out what to do here since we're already auth'd and ban check is built in"""
         pass
 
     @staticmethod
     def run(user_id: str) -> dict:
         """Get and return name, flag, meta-data, inventory, and home list for given user id."""
-        try:
-            attributes = [
-                UserAttr.NAME,
-                UserAttr.FLAG,
-                UserAttr.META,
-                UserAttr.HOMES,
-                UserAttr.INVENTORY,
-            ]
+        attributes = [
+            UserAttr.NAME,
+            UserAttr.FLAG,
+            UserAttr.META,
+            UserAttr.HOMES,
+            UserAttr.INVENTORY,
+        ]
+        response_item = User.get(user_id=user_id, attributes=", ".join(attributes))
 
-            response = User.get(user_id=user_id, attributes=", ".join(attributes))
-
-        except ClientError as e:
-            end(e.response["Error"]["Message"])  # TODO: Proper error-handling
-            # this avoids complains about unassigned reference to response_item
-            return {}
-        else:
-            if "Item" not in response or len(response["Item"] < 1):
-                end("No such user found")  # TODO: Proper error-handling
-
-            response_item = response["Item"][0]
-
-        user_data = {
+        return {
             ResponseField.User.NAME: response_item[UserAttr.NAME],
             ResponseField.User.FLAG: response_item[UserAttr.FLAG],
             ResponseField.User.META: response_item[UserAttr.META],
@@ -56,5 +44,3 @@ class Data(RequestHandler):
                 Item.get_template(value) for value in response_item[UserAttr.INVENTORY]
             ],
         }
-
-        return user_data
