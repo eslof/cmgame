f25977d8b6301fb1f2096ff897529ef42888bf1b
eslof <eslof.github@gmail.com>
2020-05-17 01:25:19 +0200

    Further queue implementation
    and I have to figure out what's changing line endings or whatever this is

--- queue/enlist.py
+++ queue/enlist.py
@@ -15,9 +15,10 @@ class Enlist(RequestHandler):
     def run(user_data: dict, user_id: str) -> bool:
         """If an enlistment for the given user_id exists then we update its timestamp.
         If there is no enlistment for the given user_id then we add one."""
-        time = datetime.now().strftime("%d-%H-%M-%S")
-        list_id = user_data[UserAttr.LIST_ID]
-        new_id = time + user_id
+        time_now = datetime.now()
+        new_id = time_now.strftime("%m-%d-%H-%M-%S") + user_id
+        list_id = user_data[UserAttr.LIST_ID] or new_id
+
         response = table.update_item(
             Key={TableKey.PARTITION: TablePartition.QUEUE, TableKey.SORT: list_id},
             UpdateExpression="SET #state = :state, #sort = :list_id",
@@ -35,11 +36,22 @@ class Enlist(RequestHandler):
         return True
 
     @staticmethod
-    def validate(user_id: str) -> QueueState:
+    def validate(user_id: str) -> dict:
         user_data = User.get(
             user_id=user_id, attributes=f"{UserAttr.QUEUE_STATE}, {UserAttr.LIST_ID}",
         )
         queue_state = QueueState(user_data[UserAttr.QUEUE_STATE])
         if queue_state == QueueState.MATCHED:
             end("Queue request API (ENLIST): Currently in a matching.")
+
+        time_now = datetime.now()
+
+        if user_data[UserAttr.LIST_ID]:
+            list_time_str = user_data[UserAttr.LIST_ID].replace(user_id, "")
+            year_str = time_now.strftime("%Y-")
+            list_time = datetime.strptime(year_str + list_time_str, "%Y-%m-%d-%H-%M-%S")
+            listing_age = int((time_now - list_time).total_seconds())
+            if listing_age < 1:
+                end("Possible misuse, client only runs this once per 5s")
+
         return user_data
