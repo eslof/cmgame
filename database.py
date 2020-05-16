340d171d229ecce3b5815e39887e4da11de6d2dd
eslof <eslof.github@gmail.com>
2020-05-16 20:23:08 +0200

    Temporary mockup for chat server endpoint
    not entierly sure how to implement this

--- database.py
+++ database.py
@@ -1,5 +1,13 @@
 import boto3
 from botocore.exceptions import ClientError
+import os
+
+
+# TODO: create cloudfront distributed s3 bucket for config data like ws url
+def web_socket_endpoint():
+    #  TODO: get live state
+    return {"response_code": 200, "address": "domain.com/ws"}
+
 
 dynamodb = boto3.resource("dynamodb")
 table = dynamodb.Table("cmgame")
