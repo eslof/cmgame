460aa77530418a9fc2446c4415eb2636204ac05f
eslof <eslof.github@gmail.com>
2020-05-17 22:53:43 +0200

    I made a router, it makes things easy
    just tag the lambda_handler(event, context): pass with @route(Routers[request], RequestType) and it's all gucci

---
+++ router.py
@@ -0,0 +1,30 @@
+from typing import Type, Callable, Any
+from internal import validate_request
+from request_handler import RequestHandler
+from user import User
+from enum import Enum
+
+
+class Route(object):
+    def __init__(self, handler: Type[RequestHandler], output: Callable):
+        self.handler = handler
+        self.output = output
+
+
+def lambda_handler(event: dict, context: Any, router: Route):
+    user_id = User.validate_id(event)
+    valid_data = router.handler.validate(event, user_id)
+    output = router.handler.run(event, user_id, valid_data)
+    router.output(output)
+
+
+def route(routers: dict, request_enum: Type[Enum]):
+    def inner(f):
+        def wrapped_f(*args):
+            req = validate_request(args[0], request_enum)
+            args = args + (routers[req])
+            return lambda_handler(*args)
+
+        return wrapped_f
+
+    return inner
