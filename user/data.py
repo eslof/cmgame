b38bbf4b3be9b29be89b5c4f165b932f40563ded
eslof <eslof.github@gmail.com>
2020-05-18 00:32:00 +0200

    Some refactoring

--- user/data.py
+++ user/data.py
@@ -11,21 +11,17 @@ class Data(RequestHandler):
     """Returning user requests a welcome package containing: Profile, inventory, home names and their biodomes."""
 
     @staticmethod
-    def run(event: dict, user_id: str, data: Any) -> Any:
-
-        user_data = User.get(user_id, UserHelper.welcome_attributes())
+    def run(event: dict, user_id: str, data: dict) -> dict:
         homes = [
             {
                 ResponseField.Home.NAME: home[UserAttr.Home.NAME],
                 ResponseField.Home.BIODOME: home[UserAttr.Home.BIODOME],
             }
-            for home in user_data[UserAttr.HOMES]
+            for home in data[UserAttr.HOMES]
         ]
-        inventory = [
-            ItemHelper.template_inv(item) for item in user_data[UserAttr.INVENTORY]
-        ]
-        return UserHelper.template_welcome(user_data, homes, inventory)
+        inventory = [ItemHelper.template_inv(item) for item in data[UserAttr.INVENTORY]]
+        return UserHelper.template_welcome(data, homes, inventory)
 
     @staticmethod
-    def validate(event: dict, user_id: str) -> Optional[dict]:
-        pass
+    def validate(event: dict, user_id: str) -> dict:
+        return User.get(user_id, UserHelper.welcome_attributes())
