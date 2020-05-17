cc5c59e48c6c5789be3293f0b1b559515a043e55
eslof <eslof.github@gmail.com>
2020-05-17 12:10:14 +0200

    lambda_function.py template supercharged
    now takes a list of requests so you make a new file lambda_function.py with service user and requests data, delete, new, save and everything is set up perfectly.

--- LambdaFunctionTemplate.py
+++ LambdaFunctionTemplate.py
@@ -1,27 +1,41 @@
-#set($sc = ${service})
-#set($r1c = ${StringUtils.removeUnderScores(${request_1})})
-#set($r1l = $request_1.toLowerCase())
-#set($r1u = $request_1.toUpperCase())
-#set($r2c = ${StringUtils.removeUnderScores(${request_2})})
-#set($r2l = $request_2.toLowerCase())
-#set($r2u = $request_2.toUpperCase())
 from default_imports import *
+#set($request = "")
+#set($sc = ${StringUtils.removeUnderScores($service)})
+#set($ns = $requests.replace(" ", ""))
+#set($rl = $ns.split(","))
+#set($rm = {})
+#set($cl = "")
+#set($i = 0)
+#foreach($request in $rl)
+    #set($rm[$request] = {})
+    #set($rm[$request].lower = $request.toLowerCase())
+    #set($rm[$request].upper = $request.toUpperCase())
+    #set($rm[$request].camel = ${StringUtils.removeUnderScores($request)})
+    #set($cl = "${cl}${rm[$request].camel}")
+    #set($i = $i + 1)
+    #if($i < $rl.size())
+        #set($cl = "${cl}, ")
+    #end
+#end##
 
-from .${r1l} import ${r1c}
-from .${r2l} import ${r2c}
+#foreach($request in $rm)
+    from .${request.lower} import ${request.camel}
+#end##
 
-assert_inheritance([${r1c}, ${r2c}], RequestHandler)
+assert_inheritance([${cl}], RequestHandler)
 
 
 @unique
 class ${sc}Request(Enum):
-    ${r1u} = auto()
-    ${r2u} = auto()
+#foreach($request in $rm)
+        ${request.upper} = auto()
+#end
 
 
 routes = {
-    ${sc}Request.$r1u: Route(${r1c}, View.generic),
-    ${sc}Request.$r2u: Route(${r2c}, View.generic)
+#foreach($request in $rm)
+    ${sc}Request.${request.upper}: Route(${request.camel}, View.generic),
+#end
 }
 
 
