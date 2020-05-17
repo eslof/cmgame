c5acca51532e65547aa753970c8938b0462b4e87
eslof <eslof.github@gmail.com>
2020-05-17 22:56:27 +0200

    Some minor changes to RequestHandler template
    now has TODO's for the docstrings to begin with as a reminder to actually implement the code

--- RequestHandlerTemplate.py
+++ RequestHandlerTemplate.py
@@ -1,17 +1,22 @@
 from typing import Optional, Any
-
 from request_handler import RequestHandler
 #set($camel_case_name = ${StringUtils.removeUnderScores($NAME)})
+#set($desc = $description)
+#set($empty = $desc.replace(" ", ""))
+#if($empty == "")
+    #set($desc = "${camel_case_name} implementation.")
+#end
+
 
 class $camel_case_name(RequestHandler):
-    """$class_doc"""
+    """TODO:${desc}"""
 
     @staticmethod
     def run(event: dict, user_id: str, data: Any) -> Any:
-        """$run_doc"""
+        """TODO:${camel_case_name}.run implementation."""
         pass
 
     @staticmethod
     def validate(event: dict, user_id: str) -> Optional[dict]:
-        """$validate_doc"""
+        """TODO:${camel_case_name}.validate implementation."""
         pass
