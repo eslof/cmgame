368fa2cc2e9be2c4633aa88a982739ba54ec0b98
eslof <eslof.github@gmail.com>
2020-05-18 22:15:07 +0200

    some thoughts as todo

--- router.py
+++ router.py
@@ -16,6 +16,7 @@ class Route(object):
 
 
 def lambda_handler(event: dict, context: Optional[Any], route: Route):
+    """Todo: this is actually part of the model even though it's routing requests... figure something out"""
     user_id = None
     if route.require_id:
         user_id = User.validate_id(event)
