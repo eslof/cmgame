d42d11c3cc1591d6df4a01faa6233882c5dd85dc
eslof <eslof.github@gmail.com>
2020-05-19 04:22:54 +0200

    clean-up

--- user/delete.py
+++ user/delete.py
@@ -7,19 +7,9 @@ class Delete(RequestHandler):
     """User requests to close the user's own account."""
 
     @staticmethod
-    def run(event: dict, user_id: str, data: Any) -> Any:
+    def run(event: dict, user_id: Optional[str], data: Optional[Any]) -> Optional[Any]:
         pass
 
     @staticmethod
-    def validate(event: dict, user_id: str) -> Optional[dict]:
-        pass
-
-    @staticmethod
-    def rufn(*args, **kwargs):
-        """TODO: research what to do"""
-        pass
-
-    @staticmethod
-    def valfidate(*args, **kwargs):
-        """Validate input phrase for given user_id."""
+    def validate(event: dict, user_id: Optional[str]) -> Optional[dict]:
         pass
