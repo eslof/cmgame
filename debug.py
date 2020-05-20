e73ca17ef383f99cc2f6e336af1632aac913047a
eslof <eslof.github@gmail.com>
2020-05-20 03:19:49 +0200

    some progress on testing
    will probably rename and move stuff around here for a bit

---
+++ debug.py
@@ -0,0 +1,55 @@
+if __debug__:
+    from enum import Enum
+    from inspect import isclass
+    from request_handler import RequestHandler
+
+    ROUTE_CLASS = None
+    LAMBDA_HANDLER_NAME = "lambda_handler"
+
+    def assert_handler(handler: type):
+        assert RequestHandler and isclass(
+            RequestHandler
+        ), f"Broken or missing RequestHandler base: '{RequestHandler}'."
+        assert handler, f"Invalid handler: '{handler}'"
+        # TODO: check if we can trick this by deriving
+        assert isclass(
+            type(handler)
+        ), f"Invalid handler: '{handler}' not instance of class."
+        assert (
+            issubclass(handler, RequestHandler)
+            and not (handler is RequestHandler)
+            and not (type(handler) is RequestHandler)
+        ), f"Invalid inheritance: '{handler}' not derive '{RequestHandler}'."
+
+    def assert_route(routes: dict, enum: Enum):
+        assert routes[
+            enum
+        ], f"Invalid route: '{routes[enum]}' in '{routes}' at '{enum}'."
+        assert isinstance(
+            routes[enum], ROUTE_CLASS
+        ), f"Invalid route: '{routes[enum]}' not instance of '{ROUTE_CLASS}' in '{routes}' at '{enum}'."
+        assert_handler(routes[enum].handler)
+        assert routes[
+            enum
+        ].output, f"Invalid output: '{routes[enum].output}' for '{routes[enum]}' in '{routes}' at '{enum}'."
+        assert callable(
+            routes[enum].output
+        ), f"Invalid output: '{routes[enum].output}' not callable for '{routes[enum]}' in '{routes}' at '{enum}'."
+
+    # todo: move to unit test (?)
+    def assert_routing(f_name: str, routes: dict, request_enum: type(Enum)):
+        assert (
+            f_name == LAMBDA_HANDLER_NAME
+        ), f"Invalid function: '{f_name}' for '{request_enum}', should be '{LAMBDA_HANDLER_NAME}'."
+        assert routes and type(routes) is dict, f"Invalid routes dict: '{routes}'."
+        assert (
+            request_enum and isclass(request_enum) and len(request_enum)
+        ), f"Invalid request_enum: '{request_enum}'"
+        for enum in routes:
+            assert (
+                enum in request_enum
+            ), f"Invalid entry: '{enum}' not in '{request_enum}'."
+            assert_route(routes, enum)
+        for enum in request_enum:
+            assert enum in routes, f"Invalid entry: '{enum}' not in '{routes}"
+            assert_route(routes, enum)
--- router.py
+++ router.py
@@ -1,9 +1,7 @@
-from inspect import isclass
-from typing import Callable, Any, Optional, Union, List
+from typing import Callable
 from internal import validate_request
 from properties import Constants
 from request_handler import RequestHandler
-from user import User
 from enum import Enum
 
 
@@ -16,61 +14,10 @@ class Route:
         self.require_id = require_id
 
 
-def _handler(event: dict, context: Optional[Any], _route: Route):
-    """Todo: this is actually part of the model even though it's routing requests... figure something out"""
-    user_id = None
-    if _route.require_id:
-        user_id = User.validate_id(event)
-    valid_data = _route.handler.validate(event, user_id)
-    output = _route.handler.run(event, user_id, valid_data or None)
-    return _route.output(output)
+if __debug__:
+    from debug import assert_routing, ROUTE_CLASS
 
-
-def assert_handler(handler: type):
-    assert RequestHandler and isclass(
-        RequestHandler
-    ), f"Broken or missing RequestHandler base: '{RequestHandler}'."
-    assert handler, f"Invalid handler: '{handler}'"
-    # TODO: check if we can trick this by deriving
-    assert isclass(
-        type(handler)
-    ), f"Invalid handler: '{handler}' not instance of class."
-    assert (
-        issubclass(handler, RequestHandler)
-        and not (handler is RequestHandler)
-        and not (type(handler) is RequestHandler)
-    ), f"Invalid inheritance: '{handler}' not derive '{RequestHandler}'."
-
-
-def assert_route(routes: dict, enum: Enum):
-    assert routes[enum], f"Invalid route: '{routes[enum]}' in '{routes}' at '{enum}'."
-    assert isinstance(
-        routes[enum], Route
-    ), f"Invalid route: '{routes[enum]}' not instance of '{Route}' in '{routes}' at '{enum}'."
-    assert_handler(routes[enum].handler)
-    assert routes[
-        enum
-    ].output, f"Invalid output: '{routes[enum].output}' for '{routes[enum]}' in '{routes}' at '{enum}'."
-    assert callable(
-        routes[enum].output
-    ), f"Invalid output: '{routes[enum].output}' not callable for '{routes[enum]}' in '{routes}' at '{enum}'."
-
-
-# todo: move to unit test (?)
-def assert_routing(f_name: str, routes: dict, request_enum: type(Enum)):
-    assert (
-        f_name == Constants.LAMBDA_HANDLER_NAME
-    ), f"Invalid function: '{f.__name__}' for '{request_enum}', should be '{Constants.LAMBDA_HANDLER_NAME}'."
-    assert routes and type(routes) is dict, f"Invalid routes dict: '{routes}'."
-    assert (
-        request_enum and isclass(request_enum) and len(request_enum)
-    ), f"Invalid request_enum: '{request_enum}'"
-    for enum in routes:
-        assert enum in request_enum, f"Invalid entry: '{enum}' not in '{request_enum}'."
-        assert_route(routes, enum)
-    for enum in request_enum:
-        assert enum in routes, f"Invalid entry: '{enum}' not in '{routes}"
-        assert_route(routes, enum)
+    ROUTE_CLASS = Route
 
 
 # TODO: collect error messages
@@ -78,7 +25,7 @@ def route(routes: dict, request_enum: type(Enum)):
     def inner(f):
         def wrapped_f(*args):
             # region Server (todo: move to unit test)
-            if True:  # __debug__
+            if __debug__:
                 assert (
                     len(args) > 0 and args[0] and type(args[0]) is dict
                 ), f"Missing argument for '{Constants.LAMBDA_HANDLER_NAME}' in '{request_enum}', should be '{Constants.LAMBDA_HANDLER_NAME}(event, context)'."
