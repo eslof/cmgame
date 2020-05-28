ead12c56f12d4f1ca69435814304d1000fdfac62
eslof <eslof.github@gmail.com>
2020-05-28 12:54:07 +0200

    more cleanup
    also working on sensible deployment script

--- view.py
+++ view.py
@@ -7,38 +7,33 @@ from properties import PacketHeader, ResponseType, ResponseField
 
 
 class View:
-    # todo: look into how a different serialization lib would fit here
-    # todo: look into typing
-
-    encode = json.dumps
+    encode = lambda value: json.dumps(value, separators=(",", ":"))
     decode = json.loads
     decode_error = JSONDecodeError
-    valid_empty = "{}"
-    separators: Tuple[str, str] = ",", ":"
 
     @classmethod
-    def serialize(cls, data: dict) -> str:
+    def serialize(cls, data: Any) -> str:
         """Serialize data using current standard format."""
-        return cls.encode(data, separators=cls.separators)
+        return cls.encode(data)
 
     @classmethod
-    def deserialize(cls, data: Optional[str]) -> Dict[str, Any]:
+    def deserialize(cls, data: str) -> Dict[str, Any]:
         """Deserialize data using current standard format."""
-        return cls.decode(data)
+        return cls.decode(data)  # type: Dict[str, Any]
 
     @classmethod
-    def response(cls, response_type: Enum, data: dict) -> str:
+    def response(cls, response_type: Enum, data: Dict[str, Any]) -> str:
         """Create and return a .serialize'd response of given type with given data."""
         data[PacketHeader.RESPONSE] = response_type
         return cls.serialize(data)
 
     @classmethod
-    def generic(cls, result: Optional[bool]) -> str:
+    def generic(cls, result: bool) -> str:
         """Create and return a .serialize'd boolean response as per given result."""
         return cls.serialize(
             {
                 PacketHeader.RESPONSE: ResponseType.GENERIC.value,
-                ResponseField.Generic.RESULT: False if result is None else result,
+                ResponseField.Generic.RESULT: result,
             }
         )
 
