c5b3d5663756ca015f495cdd33303a84d09692de
eslof <eslof.github@gmail.com>
2020-06-06 23:00:20 +0200

    starting some work on deployment code
    will zip all the appropriate services individually and put root files into a /python/ zip hierarchy as requested by AWS layer upload

---
+++ build_deployment.py
@@ -0,0 +1,26 @@
+import os
+import zipfile as zf, sys
+import shutil
+
+ls = os.listdir(".")
+
+module_list = [
+    name
+    for name in ls
+    if name
+    not in ("test", "tests", ".mypy_cache", "bin", ".git", ".idea", "__pycache__")
+    and os.path.isdir(os.path.join(".", name))
+]
+
+for module in module_list:
+    shutil.make_archive(f"bin\\{module}", "zip", module)
+
+z = zf.ZipFile("bin/layer.zip", "a")
+for name in ls:
+    if (
+        name.endswith((".py", ".json", ".sql", ".sqlite"))
+        and not name.endswith("Template.py")
+        and name != "build_deployment.py"
+    ):
+        z.write(name, f"/python/{name}")
+z.close()
