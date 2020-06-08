96885cdbf7eda990b01946637e7dcb646b1f0cbd
eslof <eslof.github@gmail.com>
2020-06-08 04:39:23 +0200

    build settings moved to build properties
    types, excludes, might replace this with gitignore parser ignorance in the future

--- build_deployment.py
+++ build_deployment.py
@@ -1,44 +1,36 @@
 import os
-from zipfile import ZipFile, ZIP_LZMA, ZIP_DEFLATED
+from zipfile import ZipFile, ZIP_DEFLATED, ZIP_STORED
 
 # region Set-up
-build_folder = "build"
-layer_file_name = "layer"
+from build_properties import (
+    DIR_IGNORE,
+    BUILD_FOLDER,
+    EXT_INCLUDE,
+    EXT_EXCLUDE,
+    LAYER_NAME,
+)
+
 ls = os.listdir(".")
-layer_file_ext_include = (".py", ".json", ".sql", ".sqlite")
-layer_file_ext_exclude = ("Template.py", "deployment.py")
-folder_ignore = [
-    "test",
-    "tests",
-    ".mypy_cache",
-    "build",
-    ".git",
-    ".idea",
-    "__pycache__",
-]
 # endregion
 # region Zip Lambda Functions
 module_list = [
     name
     for name in ls
-    if name not in folder_ignore and os.path.isdir(os.path.join(".", name))
+    if name not in DIR_IGNORE and os.path.isdir(os.path.join(".", name))
 ]
 print(module_list)
 for module in module_list:
-    with ZipFile(
-        f"{build_folder}/{module}.zip", "x", ZIP_DEFLATED, compresslevel=9
-    ) as zf:
+    with ZipFile(f"{BUILD_FOLDER}/{module}.zip", "x", ZIP_STORED) as zf:
         for dirname, subdirs, files in os.walk(module):
-            if dirname in folder_ignore or "__pycache__" in dirname:
+            if dirname in DIR_IGNORE or any(
+                DIR_IGNORE[i] in dirname for i in range(len(DIR_IGNORE))
+            ):
                 continue
 
             stripped_dir = dirname[len(module) + 1 :]
             if stripped_dir:
                 zf.write(dirname, stripped_dir)
             for filename in files:
-                if "__pycache__" in filename:
-                    continue
-                print(filename)
                 zf.write(
                     os.path.join(dirname, filename),
                     os.path.join(stripped_dir, filename),
@@ -46,13 +38,10 @@ for module in module_list:
 # endregion
 # region Zip Lambda Layer
 layer_files = [
-    name
-    for name in ls
-    if name.endswith(layer_file_ext_include)
-    and not name.endswith(layer_file_ext_exclude)
+    name for name in ls if name.endswith(EXT_INCLUDE) and not name.endswith(EXT_EXCLUDE)
 ]
 with ZipFile(
-    f"{build_folder}/{layer_file_name}.zip", "x", ZIP_DEFLATED, compresslevel=9
+    f"{BUILD_FOLDER}/{LAYER_NAME}.zip", "x", ZIP_DEFLATED, compresslevel=9
 ) as z:
     all([z.write(name, f"/python/{name}")] for name in layer_files)
 # endregion
---
+++ build_properties.py
@@ -0,0 +1,13 @@
+BUILD_FOLDER = "build"
+LAYER_NAME = "layer"
+EXT_INCLUDE = (".py", ".json", ".sql", ".sqlite")
+EXT_EXCLUDE = ("Template.py", "build_deployment.py", "build_properties.py")
+DIR_IGNORE = [
+    "test",
+    "tests",
+    ".mypy_cache",
+    "build",
+    ".git",
+    ".idea",
+    "__pycache__",
+]
