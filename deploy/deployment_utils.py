0304229480bb3f23e72c9ccd013ddc6c65156d97
eslof <65259760+eslof@users.noreply.github.com>
2020-06-09 05:48:48 +0200

    Update deployment_utils.py

--- deploy/deployment_utils.py
+++ deploy/deployment_utils.py
@@ -11,13 +11,13 @@ LAMBDA_CLIENT = boto3.client("lambda")
 
 def lambda_try(function, *args, **kwargs) -> Optional[Dict[str, Any]]:
     try:
-        results = function(*args, **kwargs)
+        results = function(args, kwargs)
     except ClientError as e:
         return None
     except Exception:
         return None
     else:
-        return results
+        return results or {}
 
 
 def get_layers() -> Dict[str, str]:
@@ -29,7 +29,7 @@ def get_layers() -> Dict[str, str]:
         all(key in layer.keys() for layer in results["Layers"])
         for key in ("LayerName", "LayerArn")
     ):
-        print(f"Unexpected contents: {results}")
+        print(f"Unexpected results: {results}")
         quit()
     layers = results["Layers"]
     if len(layers) == 0:
@@ -46,7 +46,7 @@ def get_functions() -> Dict[str, str]:
         all(key in function.keys() for function in results["Functions"])
         for key in ("FunctionName", "FunctionArn")
     ):
-        print(f"Unexpected contents: {results}")
+        print(f"Unexpected results: {results}")
         quit()
     functions = results["Functions"]
     if len(functions) == 0:
